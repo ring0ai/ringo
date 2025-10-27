d501adbf3ac08fff75cc56f9a0dfd6d91bee5432
supriyo <hisupriyooo@gmail.com>
2025-10-27 23:20:17 +0530

    feat: complete websocket setup

--- server.ts
+++ server.ts
@@ -6,6 +6,8 @@ import express from "express";
 import next from "next";
 import dotenv from "dotenv";
 import auth from "basic-auth";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 dotenv.config();
 
@@ -15,7 +17,7 @@ const handle = nextApp.getRequestHandler();
 
 const queue = new Queue("callWorker");
 const serverAdapter = new ExpressAdapter();
-serverAdapter.setBasePath("/admin/queues"); // VERY IMPORTANT
+serverAdapter.setBasePath("/admin/queues");
 
 const { addQueue, removeQueue, setQueues, replaceQueues } = createBullBoard({
   queues: [new BullAdapter(queue)],
@@ -25,15 +27,16 @@ const { addQueue, removeQueue, setQueues, replaceQueues } = createBullBoard({
 async function main() {
   await nextApp.prepare();
 
-  const server = express();
-  server.use(
+  const app = express();
+  const server = createServer(app);
+
+  //NOTE: this part manages blll queues
+  app.use(
     "/admin/queues",
     async (req, res, next) => {
       const user = auth(req);
-
       const username = process.env.BULL_QUEUE_USERNAME;
       const password = process.env.BULL_QUEUE_PASSWORD;
-
       if (!user || user.name !== username || user.pass !== password) {
         res.set("WWW-Authenticate", 'Basic realm="Admin Area"');
         return res.status(401).send("Authentication required.");
@@ -43,10 +46,37 @@ async function main() {
     serverAdapter.getRouter(),
   );
 
-  server.use((req, res) => handle(req, res));
+  app.get("/api/health", (req, res) => {
+    res.json({ status: "ok" });
+  });
+
+  //NOTE: web this part manages next js
+  app.use((req, res) => handle(req, res));
+
+  //NOTE: web sockets
+  const io = new Server(server, {
+    path: "/api/socket",
+    cors: {
+      origin: "*",
+    },
+  });
+  io.on("connection", (socket) => {
+    console.log("🔌 Socket connected:", socket.id);
+
+    socket.emit("message", "Connected!");
+
+    socket.on("ping", (msg) => {
+      console.log("Received ping:", msg);
+      socket.emit("pong", `Pong: ${msg}`);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("❌ Socket disconnected:", socket.id);
+    });
+  });
 
   const port = process.env.PORT || 3000;
-  server.listen(port, () => {
+  app.listen(port, () => {
     console.log("next js running withtin express");
   });
 }
